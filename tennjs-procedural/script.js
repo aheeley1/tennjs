a83f8a6d8229a5e1013f77e21f32b6b80dacf4cb
Adam Heeley <adamheeley@Adams-MacBook-Air.local>
2025-02-14 09:15:44 -0700

    Refactor to draw method

--- tennjs-procedural/script.js
+++ tennjs-procedural/script.js
@@ -3,14 +3,22 @@ let ctx = canvas.getContext("2d"); //get canvas's drawing context
 let width = canvas.width;
 let height = canvas.height;
 
-ctx.fillStyle = "black";
-ctx.fillRect(0,0,width,height);
+
 
 const BALL_SIZE = 5; //tue const identintifier
 let ballPosition = {x:30,y:30};
 
-ctx.fillStyle = "white";
-ctx.fillRect(ballPosition.x,ballPosition.y,BALL_SIZE,BALL_SIZE);
 
+//refactoring- changing the code without changing the behavior
+//maintainability, readability
+function draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,width,height);
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(ballPosition.x,ballPosition.y,BALL_SIZE,BALL_SIZE);
+}
+
+draw();
 
 
